b4b863342a4d156f126807027ff27812fbdc7e4c
0xswag <0xswag@users.noreply.github.com>
2025-11-07 17:58:30 +0100

    dns

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -4,7 +4,26 @@
 	networking = {
 		hostName = hostname;
 
-		networkmanager.enable = true;
+		networkmanager = {
+			enable = true;
+			dns = "systemd-resolved";
+		};
+		nameservers = [
+			"45.90.28.0#374e98.dns.nextdns.io"
+			"45.90.30.0#374e98.dns.nextdns.io"
+		];
 #		wireless.enable = true;
+	};
+
+	services.resolved = {
+		enable = true;
+		dnsovertls = "true";
+		domains = [
+			"~."
+		];
+		fallbackDns = [
+			""
+		];
+		llmnr = "false";
 	};
 }
