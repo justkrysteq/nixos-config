f7388a32b3b524af5db43f6932aa41c1208aa54d
Krysteq <justkrysteq@proton.me>
2025-11-17 12:23:49 +0100

    feat(yazi): add compress plugin

--- modules/system/programs/yazi/default.nix
+++ modules/system/programs/yazi/default.nix
@@ -14,6 +14,7 @@
 			inherit (pkgs.yaziPlugins) restore;
 			inherit (pkgs.yaziPlugins) mount;
 			inherit (pkgs.yaziPlugins) relative-motions;
+			inherit (pkgs.yaziPlugins) compress;
 			# inherit (pkgs.yaziPlugins) mediainfo;
 		};
 
@@ -23,7 +24,9 @@
 					append_keymap = [
 						{ on = [ "g"  "n" ]; run = "cd ${flake-dir}"; desc = "Go ${flake-dir}"; }
 						{ on = [ "g"  "r" ]; run = "cd /"; desc = "Go /"; }
+						{ on = [ "g"  "p" ]; run = "cd $HOME/Documents/Projects"; desc = "Go ~/Documents/Projects"; }
 						{ on = [ "R" "b" ]; run = "plugin recycle-bin"; desc = "Open Recycle Bin menu"; }
+						# { on = [ "R" "m" ]; run = "plugin mediainfo -- toggle-metadata"; desc = "Toggle media preview metadata"; }
 						{ on = "u"; run = "plugin restore"; desc = "Restore last deleted files/folders"; }
 						{ on = "U"; run = "plugin restore -- --interactive"; desc = "Restore deleted files/folders (Interactive)"; }
 						{ on = "M"; run = "plugin mount"; desc = "Open Mount menu"; }
@@ -31,6 +34,13 @@
 						{ on = [ "m" "c" ]; run = "linemode size_and_btime"; desc = "Linemode: size and creation time"; }
 						{ on = [ "g" "t" ]; run = "tab_switch 1 --relative"; desc = "Switch to next tab"; }
 						{ on = [ "g" "T" ]; run = "tab_switch -1 --relative"; desc = "Switch to previous tab"; }
+						{ on = [ "C" "a" ]; run = "plugin compress"; desc = "Archive selected files"; }
+						{ on = [ "C" "p" ]; run = "plugin compress -p"; desc = "Archive selected files (password)"; }
+						{ on = [ "C" "h" ]; run = "plugin compress -ph"; desc = "Archive selected files (password+header)"; }
+						{ on = [ "C" "l" ]; run = "plugin compress -l"; desc = "Archive selected files (compression level)"; }
+						{ on = [ "C" "u" ]; run = "plugin compress -phl"; desc = "Archive selected files (password+header+level)"; }
+						{ on = [ "C" "7" ]; run = "plugin compress 7z"; desc = "Archive selected files to 7z"; }
+						{ on = [ "C" "r" ]; run = "plugin compress rar"; desc = "Archive selected files to rar"; }
 					];
 
 					prepend_keymap = [
@@ -46,6 +56,20 @@
 						{ on = "0"; run = "plugin relative-motions 0"; desc = "Move in relative steps"; }
 					];
 				};
+
+				# plugin = {
+				# 	prepend_preloaders = [
+				# 		{ mime = "{audio,video,image}/*"; run = "mediainfo"; }
+				# 		{ mime = "application/subrip"; run = "mediainfo"; }
+				# 		{ mime = "application/postscript"; run = "mediainfo"; }
+				# 	];
+				#
+				# 	prepend_previewers = [
+				# 		{ mime = "{audio,video,image}/*"; run = "mediainfo"; }
+				# 		{ mime = "application/subrip"; run = "mediainfo"; }
+				# 		{ mime = "application/postscript"; run = "mediainfo"; }
+				# 	];
+				# };
 			};
 		};
 	};
