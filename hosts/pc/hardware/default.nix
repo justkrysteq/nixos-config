cc7ee5bed29039ca4279a9d0daeeb08b9b8444c3
Krysteq <justkrysteq@proton.me>
2025-12-09 01:49:55 +0100

    fix(pc): random system freezes

--- hosts/pc/hardware/default.nix
+++ hosts/pc/hardware/default.nix
@@ -1,8 +1,10 @@
 { ... }:
 
 {
-    imports = [
-        ./drivers.nix
-        ./hardware-configuration.nix
-    ];
+	imports = [
+		./drivers.nix
+		./hardware-configuration.nix
+	];
+
+	boot.kernelParams = [ "nvidia_drm.fbdev=1" "nvidia-drm.modeset=1" ]; # Hopefully a fix for random freezes
 }
