78fc4a25e16bd2f5f4dfe123a58bfd7ba7b65311
Krysteq <justkrysteq@proton.me>
2025-12-10 16:21:29 +0100

    chore: flake update

--- modules/system/kernel.nix
+++ modules/system/kernel.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }:
-
+{ pkgs, hostname, ... }:
+let
+	isPC = if hostname == "pc" then true else false;
+in
 {
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = if isPC then pkgs.linuxPackages_latest else pkgs.linuxKernel.packages.linux_6_17;
 }
