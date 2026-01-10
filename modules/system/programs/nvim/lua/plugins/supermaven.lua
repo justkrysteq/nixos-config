6ba6e61dd4d3c0be157799447e63b7c94cafc61b
Krysteq <justkrysteq@proton.me>
2026-01-10 19:41:58 +0100

    fix(nvim): loop loading nvim-cmp

--- modules/system/programs/nvim/lua/plugins/supermaven.lua
+++ modules/system/programs/nvim/lua/plugins/supermaven.lua
@@ -1,5 +1,6 @@
 return {
 	"supermaven-inc/supermaven-nvim",
+	event = "VeryLazy",
 	config = function()
 		require("supermaven-nvim").setup({
 			keymaps = {
@@ -13,7 +14,7 @@ return {
 			-- 	cterm = 244,
 			-- },
 			-- log_level = "info", -- set to "off" to disable logging completely
-			-- disable_inline_completion = false, -- disables inline completion for use with cmp
+			disable_inline_completion = false, -- disables inline completion for use with cmp
 			-- disable_keymaps = false, -- disables built in keymaps for more manual control
 			-- condition = function()
 			-- 	return false
