891404ff389b5ae4a524737c658bd3fbcf59c9e2
Krysteq <justkrysteq@proton.me>
2026-04-22 12:45:30 +0200

    chore: flake update

--- modules/system/overlays/elephant.nix
+++ modules/system/overlays/elephant.nix
@@ -1,26 +1,6 @@
 final: prev:
 {
 	elephant = prev.elephant.override {
-		enableArchLinuxPkgs = false;
-		enableDnfPackages = false;
-		enable1password = false;
-		enableBitwarden = false;
-		enableBluetooth = false;
-		enableBookmarks = false;
-		enableCalc = true;
-		enableClipboard = false;
-		enableDesktopApplications = true;
-		enableFiles = false;
-		enableMenus = false;
-		enableNiriActions = false;
-		enableNiriSessions = false;
-		enableProviderList = true;
-		enableRunner = false;
-		enableSnippets = false;
-		enableSymbols = false;
-		enableTodo = false;
-		enableUnicode = false;
-		enableWebsearch = false;
-		enableWindows = false;
+		enabledProviders = [ "calc" "desktopapplications" "providerlist" ];
 	};
 }
